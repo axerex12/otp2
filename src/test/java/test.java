ec4e80726bca2d1c418ac0171a86760140679c2f
axerex12 <axel.aaldijk12@gmail.com>
2025-10-23 14:36:52 +0300

    AAASSSSS

---
+++ src/test/java/test.java
@@ -0,0 +1,19 @@
+import static org.junit.Assert.assertEquals;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+import org.junit.Test;
+
+public class test {
+
+
+    @Test
+    public void totalTest() {
+        assertEquals(0, 0);
+
+    }
+
+    
+    
+}
